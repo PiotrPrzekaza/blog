865ae5ab641c45e5b28207722aae346d08b6e1e0
Piotr Przekaza <piotr.przekaza@hotmail.com>
2019-10-30 19:51:35 +0100

    added new pages to project

---
+++ src/pages/about.js
@@ -0,0 +1,16 @@
+import React from "react"
+
+import Layout from "../components/layout"
+import SEO from "../components/seo"
+
+const AboutPage = () => (
+    <Layout>
+        <SEO title="O Mnie" />
+        <h1>Piotr Przekaza</h1>
+        <h2>O mnie</h2>
+        <p>Amator programista , pasjonat, student informatyki</p>
+
+    </Layout>
+)
+
+export default AboutPage
