169747939f553c3cbf1744cf8d87baf0ef966037
Piotr Przekaza <piotr.przekaza@hotmail.com>
2019-11-04 19:50:43 +0100

    added Post component, makeed query for GQL and put posts in home page

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,6 +2,8 @@ import React from "react"
 
 import Layout from "../components/layout"
 import SEO from "../components/seo"
+import { graphql, StaticQuery } from "gatsby"
+import Post from '../components/Post'
 
 const IndexPage = () => (
   <Layout>
@@ -9,7 +11,42 @@ const IndexPage = () => (
     <h1>Programista bez Kodu</h1>
     <p>Witam na moim blogu!</p>
     <p>Czytaj, ucz się , zostaw komentarz!</p>
+    <StaticQuery query={indexQuery} render={data => {
+      return (
+        <div>
+          {data.allMarkdownRemark.edges.map(({ node }) => (
+            <Post
+              title={node.frontmatter.title}
+              author={node.frontmatter.author}
+              date={node.frontmatter.date}
+              path={node.frontmatter.path}
+              body={node.excerpt}
+            />
+          ))}
+        </div>
+      )
+    }} />
   </Layout>
 )
+
+const indexQuery = graphql`
+query{
+  allMarkdownRemark(sort: {fields: [frontmatter___date], order: DESC}) {
+    edges{
+      node{
+        id
+        frontmatter{
+          title
+          date(formatString: "MMM Do YYYY")
+          author
+          path
+        }
+        excerpt
+      }
+    }
+  }
+}
+`
+
 
 export default IndexPage
