7f0b29989e7e48e4104794e5a139770b3ae29955
Piotr Przekaza <piotr.przekaza@hotmail.com>
2019-11-11 18:43:54 +0100

    added template a single post, changed schima and fillup gatsb nod config to make a pages

--- src/pages/index.js
+++ src/pages/index.js
@@ -24,7 +24,7 @@ const IndexPage = () => (
                   title={node.frontmatter.title}
                   author={node.frontmatter.author}
                   date={node.frontmatter.date}
-                  path={node.frontmatter.path}
+                  slug={node.fields.slug}
                   body={node.excerpt}
                   fluid={node.frontmatter.image.childImageSharp.fluid}
                   tags={node.frontmatter.tags}
@@ -51,7 +51,6 @@ query{
           title
           date(formatString: "MMM Do YYYY")
           author
-          path
           tags
           image{
             childImageSharp{
@@ -61,6 +60,9 @@ query{
             }
           }
         }
+        fields{
+                slug
+            }
         excerpt
       }
     }
