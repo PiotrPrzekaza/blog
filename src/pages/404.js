768e59b6b628dc625b3c2157f7c37fa8408035f1
Piotr Przekaza <piotr.przekaza@hotmail.com>
2019-11-12 20:19:56 +0100

    added title and home button on 404 page

--- src/pages/404.js
+++ src/pages/404.js
@@ -2,11 +2,12 @@ import React from "react"
 
 import Layout from "../components/layout"
 import SEO from "../components/seo"
+import { Link } from "gatsby"
 
 const NotFoundPage = () => (
-  <Layout pageTitle="NOT FOUND">
+  <Layout pageTitle="Upss.. Nie wyprodukowałem jescze tego!!!">
     <SEO title="404: Not found" />
-    <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
+    <Link className="btn btn-primary text--uppercase" to={'/'}>Wróć Do Blogu</Link>
   </Layout>
 )
 
