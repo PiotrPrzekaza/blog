544da987e593fa6fe6b674b38897459a74985074
Piotr Przekaza <piotr.przekaza@hotmail.com>
2019-11-12 18:41:54 +0100

    maked slug to use title a blog post

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,4 +1,6 @@
+const path = require('path');
 const { slugify } = require('./src/util/utilityFunctions');
+
 
 exports.onCreateNode = ({ node, actions }) => {
     const { createNodeField } = actions
@@ -10,4 +12,38 @@ exports.onCreateNode = ({ node, actions }) => {
             value: slugFromTitle
         })
     }
+}
+
+exports.createPages = ({ actions, graphql }) => {
+    const { createPage } = actions;
+    const singlePostTemplate = path.resolve('src/templates/single-post.js')
+    return graphql(`
+            {
+                allMarkdownRemark{
+                    edges{
+                        node{
+                            frontmatter{
+                                author
+                            }
+                            fields{
+                                slug
+                            }
+                        }
+                    }
+                }
+    }`).then(res => {
+        if (res.errors) return Promise.reject(res.errors)
+
+        const posts = res.data.allMarkdownRemark.edges
+
+        posts.forEach(({ node }) => {
+            createPage({
+                path: node.fields.slug,
+                component: singlePostTemplate,
+                context: {
+                    slug: node.fields.slug
+                },
+            })
+        })
+    })
 }
--- src/templates/single-post.js
+++ src/templates/single-post.js
@@ -8,30 +8,32 @@ import Img from 'gatsby-image'
 import { slugify } from '../util/utilityFunctions'
 
 
-const SinglePost = () => {
+const SinglePost = ({ post, data }) => {
     return (
         <Layout>
             <SEO title={post.title} />
             <h1>{post.title}</h1>
             <Row>
                 <Col md="8">
-                    <Img className="card-image-top" fluid={post.image.childImageSharp.fluid} />
-                    <CardBody>
-                        <CardSubtitle>
-                            <span className="text-info">{date}</span> by{' '}
-                            <span className="text-info">{author}</span>
-                        </CardSubtitle>
-                        <div dangerouslySetInnerHTML={{ __html: data.markdownRemark.html }} />
-                        <ul className="post-tags">
-                            {post.tags.map(tag => (
-                                <li key={tag}>
-                                    <Link to={`/tag/${slugify(tag)}`}>
-                                        <Badge color="primary">{tag}</Badge>
-                                    </Link>
-                                </li>
-                            ))}
-                        </ul>
-                    </CardBody>
+                    <Card>
+                        <Img className="card-image-top" fluid={post.image.childImageSharp.fluid} />
+                        <CardBody>
+                            <CardSubtitle>
+                                <span className="text-info">{post.date}</span> by{' '}
+                                <span className="text-info">{post.author}</span>
+                            </CardSubtitle>
+                            <div dangerouslySetInnerHTML={{ __html: data.markdownRemark.html }} />
+                            <ul className="post-tags">
+                                {post.tags.map(tag => (
+                                    <li key={tag}>
+                                        <Link to={`/tag/${slugify(tag)}`}>
+                                            <Badge color="primary">{tag}</Badge>
+                                        </Link>
+                                    </li>
+                                ))}
+                            </ul>
+                        </CardBody>
+                    </Card>
                 </Col>
                 <Col md="4">
                     <Sidebar />
@@ -42,7 +44,7 @@ const SinglePost = () => {
 }
 export const postQuery = graphql`
     query blogPostBySlug($slug: String!) {
-        markdownRemark(fields: { slug: { eq:$slug }}){
+        markdownRemark(fields: { slug: { eq:$slug } }){
             id
             html
             frontmatter{
