0a882d0796296df0a33dd02ee3c5f866c9db9735
Piotr Przekaza <piotr.przekaza@hotmail.com>
2019-11-13 16:03:51 +0100

    added author bio in util folder to pass info to sidebar for each post

---
+++ src/templates/authors.js
@@ -0,0 +1,9 @@
+const authors = [
+    {
+        name: 'Piotr Przekaza',
+        imageUrl: 'me.jpg',
+        bio: 'Javascript Developer - programista amator, samouk. Student I roku informatyki na PWSIP w Łomży.'
+    }
+]
+
+module.exports = authors
