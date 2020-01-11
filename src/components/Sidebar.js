f5c66e2e1bc78eaaaab54e7d58d95ba68619719b
Piotr Przekaza <piotr.przekaza@hotmail.com>
2020-01-11 20:28:09 +0100

    deleted mailing component

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -1,120 +1,163 @@
-import React from 'react'
-import { Card, CardTitle, CardBody, CardText, Form, FormGroup, Input } from 'reactstrap'
-import { graphql, StaticQuery, Link } from 'gatsby'
-import Img from 'gatsby-image'
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faFacebookF, faInstagram, faTwitter, faGithub, faLinkedin, } from '@fortawesome/free-brands-svg-icons'
-
-
+import React from "react"
+import {
+  Card,
+  CardTitle,
+  CardBody,
+  CardText,
+  Form,
+  FormGroup,
+  Input,
+} from "reactstrap"
+import { graphql, StaticQuery, Link } from "gatsby"
+import Img from "gatsby-image"
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
+import {
+  faFacebookF,
+  faInstagram,
+  faTwitter,
+  faGithub,
+  faLinkedin,
+} from "@fortawesome/free-brands-svg-icons"
 
 const Sidebar = ({ author, authorFluid }) => (
-    <div>
-        {author && (
-            <Card>
-                <Img className="card-image-top" fluid={authorFluid} />
-                <CardBody>
-                    <CardTitle className="text-uppercase text-center mb-3">{author.name}</CardTitle>
-                    <CardText>{author.bio}</CardText>
-                    <div className="author-social-links text-center">
-                        <ul className="social-links-list">
-                            <li><a href={author.facebook} target="_blank" rel="noopener noreferrer" className="facebook">
-                                <FontAwesomeIcon icon={faFacebookF} />
-                            </a></li>
-                            <li><a href={author.instagram} target="_blank" rel="noopener noreferrer" className="instagram">
-                                <FontAwesomeIcon icon={faInstagram} />
-                            </a></li>
-                            <li><a href={author.twitter} target="_blank" rel="noopener noreferrer" className="twitter">
-                                <FontAwesomeIcon icon={faTwitter} />
-                            </a></li>
-                            <li><a href={author.github} target="_blank" rel="noopener noreferrer" className="github">
-                                <FontAwesomeIcon icon={faGithub} />
-                            </a></li>
-                            <li><a href={author.linkedin} target="_blank" rel="noopener noreferrer" className="linkedin">
-                                <FontAwesomeIcon icon={faLinkedin} />
-                            </a></li>
-                        </ul>
-                    </div>
-                </CardBody>
-            </Card>
-        )}
-        <Card>
-            <CardBody>
-                <CardTitle className="text-center text-uppercase mb-3">
-                    Newsletter
-                </CardTitle>
-                <Form className="text-center">
-                    <FormGroup>
-                        <Input type="email" name="email" placeholder="Your address@email..." />
-                    </FormGroup>
-                    <button className="btn btn-outline-success text-uppercase">
-                        Subskrybuj
-                    </button>
-                </Form>
-            </CardBody>
-        </Card>
-        <Card>
-            <CardBody>
-                <CardTitle className="text-uppercase text-center">
-                    Miejsce na Reklame
-                </CardTitle>
-                <img src="https://via.placeholder.com/320x200" alt="Advert" style={{ width: "100%" }} />
-            </CardBody>
-        </Card>
-        <Card>
-            <CardBody>
-                <CardTitle className="text-center text-uppercase md-3">
-                    Ostatnio na blogu
-                </CardTitle>
-                <StaticQuery query={sidebarQuery} render={(data) => (
-                    <div>
-                        {data.allMarkdownRemark.edges.map(({ node }) => (
-                            <Card key={node.id}>
-                                <Link to={node.fields.slug}>
-                                    <Img className="card-image-top" fluid={node.frontmatter.image.childImageSharp.fluid} />
-                                </Link>
-                                <CardBody>
-                                    <CardTitle>
-                                        <Link to={node.fields.slug} >
-                                            {node.frontmatter.title}
-                                        </Link>
-                                    </CardTitle>
-                                </CardBody>
-                            </Card>
-                        ))}
-                    </div>
-                )} />
-            </CardBody>
-        </Card>
-    </div>
+  <div>
+    {author && (
+      <Card>
+        <Img className="card-image-top" fluid={authorFluid} />
+        <CardBody>
+          <CardTitle className="text-uppercase text-center mb-3">
+            {author.name}
+          </CardTitle>
+          <CardText>{author.bio}</CardText>
+          <div className="author-social-links text-center">
+            <ul className="social-links-list">
+              <li>
+                <a
+                  href={author.facebook}
+                  target="_blank"
+                  rel="noopener noreferrer"
+                  className="facebook"
+                >
+                  <FontAwesomeIcon icon={faFacebookF} />
+                </a>
+              </li>
+              <li>
+                <a
+                  href={author.instagram}
+                  target="_blank"
+                  rel="noopener noreferrer"
+                  className="instagram"
+                >
+                  <FontAwesomeIcon icon={faInstagram} />
+                </a>
+              </li>
+              <li>
+                <a
+                  href={author.twitter}
+                  target="_blank"
+                  rel="noopener noreferrer"
+                  className="twitter"
+                >
+                  <FontAwesomeIcon icon={faTwitter} />
+                </a>
+              </li>
+              <li>
+                <a
+                  href={author.github}
+                  target="_blank"
+                  rel="noopener noreferrer"
+                  className="github"
+                >
+                  <FontAwesomeIcon icon={faGithub} />
+                </a>
+              </li>
+              <li>
+                <a
+                  href={author.linkedin}
+                  target="_blank"
+                  rel="noopener noreferrer"
+                  className="linkedin"
+                >
+                  <FontAwesomeIcon icon={faLinkedin} />
+                </a>
+              </li>
+            </ul>
+          </div>
+        </CardBody>
+      </Card>
+    )}
+    <Card>
+      <CardBody>
+        <CardTitle className="text-center text-uppercase md-3">
+          Ostatnio na blogu
+        </CardTitle>
+        <StaticQuery
+          query={sidebarQuery}
+          render={data => (
+            <div>
+              {data.allMarkdownRemark.edges.map(({ node }) => (
+                <Card key={node.id}>
+                  <Link to={node.fields.slug}>
+                    <Img
+                      className="card-image-top"
+                      fluid={node.frontmatter.image.childImageSharp.fluid}
+                    />
+                  </Link>
+                  <CardBody>
+                    <CardTitle>
+                      <Link to={node.fields.slug}>
+                        {node.frontmatter.title}
+                      </Link>
+                    </CardTitle>
+                  </CardBody>
+                </Card>
+              ))}
+            </div>
+          )}
+        />
+      </CardBody>
+    </Card>
+    <Card>
+      <CardBody>
+        <CardTitle className="text-uppercase text-center">
+          Miejsce na Reklame
+        </CardTitle>
+        <img
+          src="https://via.placeholder.com/320x200"
+          alt="Advert"
+          style={{ width: "100%" }}
+        />
+      </CardBody>
+    </Card>
+  </div>
 )
 
 const sidebarQuery = graphql`
   query sidebarQuery {
     allMarkdownRemark(
-        sort: {fields: [frontmatter___date], order: DESC}
-        limit: 3
-        ){
-            edges{
-                node{
-                    id
-                    frontmatter{
-                    title
-                    image{
-                        childImageSharp{
-                            fluid(maxWidth: 300){
-                                ...GatsbyImageSharpFluid
-                            }
-                        }
-                    }
+      sort: { fields: [frontmatter___date], order: DESC }
+      limit: 3
+    ) {
+      edges {
+        node {
+          id
+          frontmatter {
+            title
+            image {
+              childImageSharp {
+                fluid(maxWidth: 300) {
+                  ...GatsbyImageSharpFluid
                 }
-                fields{
-                    slug
-                }
-                }
+              }
             }
+          }
+          fields {
+            slug
+          }
         }
+      }
     }
+  }
 `
 
-
-export default Sidebar;
+export default Sidebar
