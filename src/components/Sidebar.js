1d47a1368f6b2c7fb389588162b3f1097ada7dba
Piotr Przekaza <piotr.przekaza@hotmail.com>
2020-01-15 21:17:18 +0100

    fix card body

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -118,19 +118,15 @@ const Sidebar = ({ author, authorFluid }) => (
         />
       </CardBody>
     </Card>
-    <Ads />
-    <Ads
-      client="ca-pub-5106360774377239"
-      slot="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"
-      format="auto"
-    ></Ads>
-    <CardBody>
-      <Ads
-        client="ca-pub-5106360774377239"
-        slot="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"
-        format="auto"
-      ></Ads>
-    </CardBody>
+    <Card>
+      <CardBody>
+        <Ads
+          client="ca-pub-5106360774377239"
+          slot="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"
+          format="auto"
+        ></Ads>
+      </CardBody>
+    </Card>
   </div>
 )
 
